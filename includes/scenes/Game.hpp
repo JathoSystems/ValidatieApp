2af11b9e1bd0f55e24d18bca27cde1ba8817bf99
Justin9091 <jusraaijmakers@gmail.com>
2025-12-16 14:50:47 +0100

    feat/characters: bugfix

--- includes/scenes/Game.hpp
+++ includes/scenes/Game.hpp
@@ -4,11 +4,19 @@
 
 #ifndef VUURJONGEN_WATERMEISJE_GAME_GAME_HPP
 #define VUURJONGEN_WATERMEISJE_GAME_GAME_HPP
+#include "Events/EventManager.h"
+#include "Network/NetworkSystem.h"
 #include "Scenes/Scene.h"
 
 class Game : public Scene {
+private:
+    std::shared_ptr<NetworkSystem> _network;
+    EventManager *_eventManager;
+
 public:
-    Game();
+    Game(std::shared_ptr<NetworkSystem> network, EventManager *eventManager);
+
+    void onInitialRender() override;
 };
 
 #endif //VUURJONGEN_WATERMEISJE_GAME_GAME_HPP
